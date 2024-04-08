dd4f552fd568c7d5bd3d79ba01c6359652406eb3
Roman Alikevich <roma.alikevich8@gmail.com>
2024-04-08 18:56:22 +0300

    fix

--- tasks/cartesian_product.py
+++ tasks/cartesian_product.py
@@ -15,4 +15,4 @@ def cartesian_product(arr1: list[T1], arr2: list[T2]) -> list[tuple[T1, T2]]:
         >> cartesian_product([1, 2], [3, 4])
         [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
-    raise list((x, y) for x in arr1 for y in arr2)
+    return list((x, y) for x in arr1 for y in arr2)
